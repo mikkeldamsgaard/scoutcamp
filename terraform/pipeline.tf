8463802439004b0f88320a1d553a9cf2f22dacb3
Mikkel Damsgaard <mikkel@cloudpartners.com>
2018-04-20 15:00:15 +0200

    git trouble

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -81,7 +81,7 @@ resource "aws_codebuild_project" "build" {
   name = "${terraform.workspace}-build"
   source {
     type = "CODEPIPELINE"
-    buildspec = "terraform/cicd/build/buildspec.yml"
+    buildspec = "terraform/cicd/buildscripts/buildspec.yml"
   }
   service_role = "${aws_iam_role.codebuild.arn}"
 }
