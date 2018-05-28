8268d8c88907497f860e17968add7de951709c21
Mikkel Damsgaard <mikkel@cloudpartners.com>
2018-05-28 09:07:31 +0200

    Configuration updates

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -4,7 +4,7 @@ import 'dart:html';
 import 'package:scoutcamp/src/config/vars.dart';
 
 Future<bool> loggedIn() {
-  return HttpRequest.getString(server + "isLoggedIn", withCredentials: true).then((r) {
+  return HttpRequest.getString(cfg.server + "isLoggedIn", withCredentials: true).then((r) {
         return (r == "YES");
   });
 }
