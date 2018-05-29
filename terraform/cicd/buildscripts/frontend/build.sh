c8d3d5c4c22ac3f9bcd92b362a2e6e80eeca0604
Mikkel Damsgaard <mikkel@cloudpartners.com>
2018-05-29 13:02:41 +0200

    Debugging vars config in frontend

--- terraform/cicd/buildscripts/frontend/build.sh
+++ terraform/cicd/buildscripts/frontend/build.sh
@@ -5,12 +5,13 @@ set -v -e
 cd frontend
 
 j2 lib/src/config/vars.template.dart > lib/src/config/vars.dart
+cat lib/src/config/vars.dart
 
 PATH="$PATH":"~/.pub-cache/bin"
 /usr/lib/dart/bin/pub global activate pub_mediator
 mediator
 
-/usr/lib/dart/bin/pub get -v
+/usr/lib/dart/bin/pub get
 /usr/lib/dart/bin/pub build --mode=release --output=build
 cd ..
 
