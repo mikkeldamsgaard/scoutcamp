30e5a6fc3b17085c56f426aaec63ab39ef9e2b1c
Mikkel Damsgaard <mikkel@cloudpartners.com>
2018-05-29 10:26:04 +0200

    CI-CD changes

--- terraform/cicd/buildscripts/frontend/build.sh
+++ terraform/cicd/buildscripts/frontend/build.sh
@@ -8,7 +8,7 @@ PATH="$PATH":"~/.pub-cache/bin"
 /usr/lib/dart/bin/pub global activate pub_mediator
 mediator
 
-/usr/lib/dart/bin/pub get
+/usr/lib/dart/bin/pub get -v
 /usr/lib/dart/bin/pub build --mode=release --output=build
 cd ..
 
